0cf63559831f1e015f39282dc11402bb281a4eaf
Zamirathe <zamirathe@centurylink.net>
2015-04-25 15:43:41 -0600

    First Release

--- ZaupLeaderboard/Properties/AssemblyInfo.cs
+++ ZaupLeaderboard/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ZaupLeaderboard")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ZaupLeaderboard")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2015")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
